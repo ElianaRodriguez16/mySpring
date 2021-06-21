f065cd587f7da90a23e48e99a07684ce658017e8
fcoria <fcoria@FCORIA-W10.ar.lumina.net>
2021-06-21 16:14:47 -0300

    mod demo

--- src/test/demo/DemoMySpring.java
+++ src/test/demo/DemoMySpring.java
@@ -1,14 +1,16 @@
 package test.demo;
 
+import java.lang.reflect.InvocationTargetException;
+
 import myspring.ComponentScan;
 import myspring.Factory;
 
 @ComponentScan("test.demo")
 public class DemoMySpring
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException, InstantiationException
    {
-      Banda banda = Factory.getObject(TheBeatles.class);
+      Banda banda = (Banda) Factory.getObject(TheBeatles.class);
       System.out.println(banda.toString());
    }
 }
