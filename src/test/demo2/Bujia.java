1a43ff9dd77ffd057702872d2e2b9076d7954a25
fcoria <fcoria@FCORIA-W10.ar.lumina.net>
2021-06-21 16:32:51 -0300

    commit add

---
+++ src/test/demo2/Bujia.java
@@ -0,0 +1,7 @@
+package test.demo2;
+
+public interface Bujia {
+
+	public void bujear();
+
+}
---
+++ src/test/demo2/BujiaBosh.java
@@ -0,0 +1,12 @@
+package test.demo2;
+
+public class BujiaBosh implements Bujia{
+
+	@Override
+	public void bujear() {
+		System.out.println("bujeando");
+
+
+	}
+
+}
